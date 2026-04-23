c1efd7d2c4172138d6df0a5eec0689bdf0e78131
globalNail <nthnamto147@gmail.com>
2026-04-24 01:58:20 +0700

    update API and SOCKET URLs to production endpoints

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000/api";
-export const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:4000";
+export const API_BASE_URL =
+    process.env.NEXT_PUBLIC_API_BASE_URL || "https://wof-rho.vercel.app/api";
+export const SOCKET_URL =
+    process.env.NEXT_PUBLIC_SOCKET_URL || "https://wof-rho.vercel.app/socket";
